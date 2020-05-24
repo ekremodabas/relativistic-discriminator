71699a2d90167721a6440078b7bb487a868ce904
ekremodabas <citizen@outlook.com>
2020-05-24 22:44:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ if(args.seed):  # use a pre-determined seed for numpy, random and torch if args.
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-
+    if(args.cuda):
         torch.cuda.manual_seed_all(args.seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = True
